83c2aac7204aa1604c40e4e7fc9e9c7b40a1a2fb
RedBeans0 <Thunderkracker934@gmail.com>
2022-10-14 13:56:27 -0500

    Initial commit

---
+++ code-labs-FE-angular-projects/projects/c14-my-first-app/src/app/post/post.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+  post:string = "I'm a post!";
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
