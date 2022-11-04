41e8378494ef98f2e323cdacdf810ff665b4553b
RedBeans0 <Thunderkracker934@gmail.com>
2022-11-04 13:50:59 -0500

    friday
    components

---
+++ angular-basic-custom-property-exercise/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, Type } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-basic-custom-property-exercise';
+  name = 'John Deer';
+}
+
+
